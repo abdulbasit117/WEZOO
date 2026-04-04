92a5f623742a15d16d2f91c49593f843af6b771c
abdulvali <abdulvaliys@gmail.com>
2026-04-04 15:02:57 +0600

    цыпш

--- bron/wsgi.py
+++ bron/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for bron project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bron.settings')
 
 application = get_wsgi_application()
+
+# 🔥 ДОБАВЬ ЭТО НИЖЕ
+from django.core.management import call_command
+
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print("Migration error:", e)
